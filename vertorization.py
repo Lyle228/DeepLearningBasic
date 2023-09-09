705827c40e93274fef949ca5f68b4f514d86956e
minsik <rlaalstlr228@inu.ac.kr>
2023-09-09 18:00:25 +0900

    벡터화를 해야하는 이유

--- vertorization.py
+++ vertorization.py
@@ -10,7 +10,7 @@ tic = time.time()
 c = np.dot(a, b)
 toc = time.time()
 
-print("벡터화: " + str(1000*(toc-tic)) + "초")
+print("벡터화: " + str(1000*(toc-tic)) + "ms")
 
 c = 0
 tic = time.time()
@@ -18,7 +18,7 @@ for i in range(size):
     c += a[i] * b[i]
 toc = time.time()
 
-print("반복문: " + str(1000*(toc-tic)) + "초")
+print("반복문: " + str(1000*(toc-tic)) + "ms")
 
-# 사이즈 백만 기준으로 벡터곱은 평균 1초
-# 반복문을 이용한 곱은 평균 300초로 큰 성능차이를 보임.
+# 사이즈 백만 기준으로 벡터곱은 평균 1ms
+# 반복문을 이용한 곱은 평균 300ms로 큰 성능차이를 보임.
